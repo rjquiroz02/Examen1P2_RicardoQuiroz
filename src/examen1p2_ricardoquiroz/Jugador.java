df6f9e2d024512b3dca8c8aa16622576381c9749
rjquiroz02 <rjquiroz02@unitec.edu>
2022-08-19 14:23:30 -0600

    Commit#3

---
+++ src/examen1p2_ricardoquiroz/Jugador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1p2_ricardoquiroz;
+
+/**
+ *
+ * @author rjqer
+ */
+public class Jugador {
+    private String nombre, id, contrasena;
+    private Personaje personaje;
+    
+    
+}
---
+++ src/examen1p2_ricardoquiroz/Personaje.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1p2_ricardoquiroz;
+
+/**
+ *
+ * @author rjqer
+ */
+public class Personaje {
+    protected String nombre;
+    protected Arma arma;
+    protected int vida, escudo;
+    
+    
+}
